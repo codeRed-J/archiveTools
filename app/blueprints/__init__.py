bccf81fd7be2baaced5c6a02cdd82fe2a9834199
Svjatoslav Matvejev <svjatoslav.matvejev@gmail.com>
2023-03-27 16:53:09 +0300

    Delete main blueprint as there will be no public pages in application.

--- app/blueprints/__init__.py
+++ app/blueprints/__init__.py
@@ -1,8 +1,7 @@
 """Package contains application blueprints"""
 
 from app.blueprints.auth import auth
-from app.blueprints.main import main
 from app.blueprints.user import user
 
 # List of all blueprints that should be registered in application factory
-BLUEPRINTS_LIST = [auth, main, user]
+BLUEPRINTS_LIST = [auth, user]
