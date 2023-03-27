d465ed9ea57c8bdbb2ae9fd305ef17e5127d84a0
Svjatoslav Matvejev <svjatoslav.matvejev@gmail.com>
2023-03-27 13:58:00 +0300

    Add SQLAlchemy to the application.

--- app/extensions.py
+++ app/extensions.py
@@ -1,6 +1,8 @@
 from flask_bootstrap import Bootstrap5
+from flask_sqlalchemy import SQLAlchemy
 
 bootstrap = Bootstrap5()
+db = SQLAlchemy()
 
 # List of all extensions that should be initialized in application factory
-EXTENSIONS_LIST = [bootstrap]
+EXTENSIONS_LIST = [bootstrap, db]
