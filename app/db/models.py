ff751cfbd89bb2319fd08e96c56d80f4851fe252
Svjatoslav Matvejev <svjatoslav.matvejev@gmail.com>
2023-03-28 14:09:07 +0300

    Add methods to User class for registration and login purposes.

--- app/db/models.py
+++ app/db/models.py
@@ -1,7 +1,19 @@
+from datetime import datetime
+from typing import Self, Optional
+
+from werkzeug.security import generate_password_hash, check_password_hash
+from flask_login import UserMixin
+
+
 from app.extensions import db, login_manager
 
 
-class User(db.Model):
+@login_manager.user_loader
+def load_user(user_id):
+    return User.query.get(int(user_id))
+
+
+class User(UserMixin, db.Model):
     __tablename__ = "users"
 
     id = db.Column(db.Integer, primary_key=True)
@@ -13,6 +25,70 @@ class User(db.Model):
     confirmed = db.Column(db.Boolean, default=False)
 
     projects = db.relationship("Project", backref="user")
+
+    @property
+    def password(self):
+        raise AttributeError("Password attribute can't be read")
+
+    @password.setter
+    def password(self, password: str) -> None:
+        """Generates password hash
+
+        Assigns generated password hash to password_hash attribute
+
+        Args:
+            password: Password to be hashed
+        """
+        self.password_hash = generate_password_hash(password)
+
+    def verify_password(self, password: str) -> bool:
+        """Checks given password against hashed password
+
+        Args:
+            password: Password to be checked
+
+        Returns:
+             True if the password matched, False otherwise.
+        """
+        return check_password_hash(self.password_hash, password)
+
+    @classmethod
+    def email_exists(cls, email: str) -> bool:
+        """Checks if user with given email already exists
+
+        Args:
+            email: Given email to be checked
+
+        Returns:
+            True if the email exists, False otherwise.
+        """
+        if User.query.filter_by(email=email).first():
+            return True
+        return False
+
+    @classmethod
+    def create_user(cls, email: str, password: str) -> None:
+        """Creates new user
+
+        Args:
+            email: User email
+            password: User password
+        """
+        user = User(email=email, password=password, created_at=datetime.now())
+        db.session.add(user)
+        db.session.commit()
+
+    @classmethod
+    def get_user(cls, email: str) -> Optional[Self]:
+        """Get user by email
+
+        Args:
+            email: User email
+
+        Returns:
+            User instance if user with given exists, None otherwise
+        """
+        return User.query.filter_by(email=email).first()
 
 
 class Inventory(db.Model):
@@ -61,7 +137,7 @@ class ErrorMessage(db.Model):
     __tablename__ = "error_messages"
 
     id = db.Column(db.Integer, primary_key=True)
-    message = db.Columnt(db.String, nullable=False)
+    message = db.Column(db.String, nullable=False)
 
 
 
